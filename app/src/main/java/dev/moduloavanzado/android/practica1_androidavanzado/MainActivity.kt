df7bf4ed45b8f995da367e2e69395226caf4e390
Patricio Jaime Porras <patricio.jaime@rizomadev.com>
2021-03-19 19:33:30 -0600

    TextChangedListener added

--- app/src/main/java/dev/moduloavanzado/android/practica1_androidavanzado/MainActivity.kt
+++ app/src/main/java/dev/moduloavanzado/android/practica1_androidavanzado/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             if (!isPasswordValid(password.editText!!.text)) {
                 password.error = getString(R.string.shr_error_password)
+                button.isEnabled = false
             } else {
                 // Clear the error.
                 password.error = null
@@ -27,12 +29,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        password.setOnKeyListener { _, _, _ ->
+        password.editText!!.addTextChangedListener {
             if (isPasswordValid(password.editText!!.text)) {
                 // Clear the error.
                 password.error = null
+                button.isEnabled = true
             }
-            true
         }
 
     }
